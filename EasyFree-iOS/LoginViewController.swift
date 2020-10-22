8a5f077980ddcdb23eef522e3da2c256662811e9
nohgyumyung <gyumyung1995@gmail.com>
2020-10-22 20:18:50 +0900

    [ADD] 화면 전환 구현

--- EasyFree-iOS/LoginViewController.swift
+++ EasyFree-iOS/LoginViewController.swift
@@ -27,19 +27,15 @@ class LoginViewController: UIViewController {
         }
         
         if isValidUserName(userName: userName) && isValidPassword(password: password) {
-            loginSuccessAlert()
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+
+            (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
         } else {
             loginFailAlert()
         }
     }
-    
-    func loginSuccessAlert() {
-        let alert = UIAlertController(title: nil, message: "로그인 성공", preferredStyle: .alert)
-        let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-    
+
     func loginFailAlert() {
         let alert = UIAlertController(title: nil, message: "로그인 실패", preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default, handler: nil)
@@ -48,7 +44,7 @@ class LoginViewController: UIViewController {
     }
     
     func isValidUserName(userName: String) -> Bool {
-        let testUserName = "노규명"
+        let testUserName = "o"
         
         if userName == testUserName {
             return true
@@ -58,7 +54,7 @@ class LoginViewController: UIViewController {
     }
     
     func isValidPassword(password: String) -> Bool {
-        let testPassword = "12345"
+        let testPassword = "o"
         
         if password == testPassword {
             return true
