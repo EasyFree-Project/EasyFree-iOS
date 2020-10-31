94ae71a1c608ad06633977facb027641924a0ca8
nohgyumyung <gyumyung1995@gmail.com>
2020-10-31 14:48:19 +0900

    Remove check function for duplicate username

--- EasyFree-iOS/SignupViewController.swift
+++ EasyFree-iOS/SignupViewController.swift
@@ -13,30 +13,10 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var userNameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPasswordField: UITextField!
-    @IBOutlet weak var checkButton: UIButton!
     @IBOutlet weak var signupButton: UIButton!
-    
-    var isUserNameAvailable: Bool = false
-    var availableUserName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    @IBAction func checkUserName(_ sender: Any) {
-        let testUserName = "노규명"
-        
-        guard let userName = userNameField.text else {
-            return
-        }
-        
-        if (testUserName != userName) && (userName != "") {
-            availableUserNameAlert()
-            isUserNameAvailable = true
-            availableUserName = userName
-        } else {
-            notAvailableUserNameAlert()
-        }
     }
     
     @IBAction func signup(_ sender: Any) {
@@ -84,13 +64,6 @@ class SignupViewController: UIViewController {
         }
     }
     
-    func availableUserNameAlert() {
-        let alert = UIAlertController(title: nil, message: "사용 가능한 아이디입니다", preferredStyle: .alert)
-        let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-    
     func notAvailableUserNameAlert() {
         let alert = UIAlertController(title: nil, message: "사용할 수 없는 아이디입니다", preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default, handler: nil)
@@ -111,12 +84,4 @@ class SignupViewController: UIViewController {
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-    
-    func checkUserNameAlert() {
-        let alert = UIAlertController(title: nil, message: "아이디 중복확인을 해주세요", preferredStyle: .alert)
-        let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-    
 }
