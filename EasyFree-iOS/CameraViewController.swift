72fdbfd0d4bd5b302eef198581f269e53103e6de
nohgyumyung <gyumyung1995@gmail.com>
2020-11-01 21:49:09 +0900

    Implement product detection with test datas

--- EasyFree-iOS/CameraViewController.swift
+++ EasyFree-iOS/CameraViewController.swift
@@ -19,6 +19,14 @@ class CameraViewController: UIViewController {
     @IBOutlet weak var previewView: PreviewView!
     @IBOutlet weak var captureButton: UIButton!
     
+    var productPosition: [ProductPosition] = [
+        ProductPosition(x1Axis: 41, x2Axis: 103, y1Axis: 427, y2Axis: 489, categoryNumber: 6000095829),
+        ProductPosition(x1Axis: 157, x2Axis: 209, y1Axis: 255, y2Axis: 317, categoryNumber: 6000096294),
+        ProductPosition(x1Axis: 227, x2Axis: 289, y1Axis: 155, y2Axis: 217, categoryNumber: 6000096294),
+        ProductPosition(x1Axis: 27, x2Axis: 89, y1Axis: 105, y2Axis: 167, categoryNumber: 6000096294),
+        ProductPosition(x1Axis: 27, x2Axis: 89, y1Axis: 355, y2Axis: 417, categoryNumber: 6000096294)
+    ]
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -42,8 +50,33 @@ class CameraViewController: UIViewController {
         }
     }
     
+    @IBAction func recapturePhoto(_ sender: Any) {
+        for view in self.previewView.subviews{
+            view.removeFromSuperview()
+        }
+        startSession()
+    }
+    
     func sendPhoto(image: UIImage) {
-        print("captured!")
+        stopSession()
+        showButtons()
+    }
+    
+    func showButtons () {
+        for i in  0..<productPosition.count {
+            let productButton = UIButton(frame: CGRect(x: productPosition[i].x1Axis, y: productPosition[i].y1Axis, width: productPosition[i].x2Axis - productPosition[i].x1Axis, height: productPosition[i].y2Axis - productPosition[i].y1Axis))
+            productButton.layer.borderColor = UIColor.black.cgColor
+            productButton.layer.borderWidth = 3
+            productButton.setTitle("button\(i)", for: .normal)
+            productButton.setTitleColor(UIColor.clear, for: .normal)
+            productButton.addTarget(self, action: #selector(productButtonClicked), for: .touchUpInside)
+            
+            self.previewView.addSubview(productButton)
+        }
+    }
+    
+    @objc func productButtonClicked(_ sender: UIButton) {
+        print(sender.currentTitle!)
     }
 }
 
@@ -117,6 +150,22 @@ extension CameraViewController: AVCapturePhotoCaptureDelegate {
         guard let imageData = photo.fileDataRepresentation() else { return }
         guard let image = UIImage(data: imageData) else { return }
         self.sendPhoto(image: image)
+    }
+}
+
+struct ProductPosition: Codable {
+    let x1Axis: Int
+    let x2Axis: Int
+    let y1Axis: Int
+    let y2Axis: Int
+    let categoryNumber: Int
+    
+    init(x1Axis: Int, x2Axis: Int, y1Axis: Int, y2Axis: Int, categoryNumber: Int) {
+        self.x1Axis = x1Axis
+        self.x2Axis = x2Axis
+        self.y1Axis = y1Axis
+        self.y2Axis = y2Axis
+        self.categoryNumber = categoryNumber
     }
 }
 
